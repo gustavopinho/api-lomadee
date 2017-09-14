a7311615db0ef1e53ba58a1c0872ac22da39e912
Gustavo Pinho <gustavopinho@gustavopinho.com>
2017-09-14 10:33:37 -0300

    Adicionando import

--- lomadee/__init__.py
+++ lomadee/__init__.py
@@ -1,2 +1,5 @@
 def joke():
     return (u'Api de ofertas lomadee')
+
+
+from .lomadee import *
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lomadee',
-      version='0.1',
+      version='0.2',
       description='Api de ofertas lomadee',
       url='git@gitlab.com:gustavopinho/lomadee.git',
       author='Gustavo Pinho',
